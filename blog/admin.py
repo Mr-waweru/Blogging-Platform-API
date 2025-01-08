13d5ae22d3b40a4757089221cc1b099fc1e73f7e
Mr-waweru <gitaugeorge001@gmail.com>
2025-01-09 01:53:56 +0300

    Register models

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ("email", "is_staff", "is_superuser", "is_active")
+    list_filter = ("is_superuser", "is_staff", "is_active")
+
+admin.site.register(CustomUser, CustomUserAdmin)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+    list_display = ("name",)
+admin.site.register(Category)
+
+
+class TagAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+    list_filter = ("name",)
+
+admin.site.register(Tag, TagAdmin)
+admin.site.register(Post)
+admin.site.register(Comment)
+admin.site.register(Like)
+admin.site.register(Rating)
