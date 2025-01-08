644a6dc1ba582c9c139dfb6a0dec2e2cfb5ed29b
Mr-waweru <gitaugeorge001@gmail.com>
2025-01-08 15:03:18 +0300

    Serialize like and rating

--- blog/serializers.py
+++ blog/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Post, Comment, Tag, Category
+from .models import Post, Comment, Tag, Category, Like, Rating
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
@@ -34,7 +34,7 @@ class PostSerializer(serializers.ModelSerializer):
         model = Post
         fields = [
             "id", "title", "content", "author", "category", "tags",
-            "published_date", "created_date", "updated", "comments"
+            "published_date", "updated", "comments"
         ]
 
 
@@ -46,3 +46,31 @@ class PostCreateUpdateSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
         fields = ["title", "content", "category", "tags"]
+
+
+class LikeSerializer(serializers.ModelSerializer):
+    user = serializers.StringRelatedField(read_only=True)  # Display user's username
+    post_title = serializers.CharField(source="post.title", read_only=True)  # Include post title in the response
+
+    class Meta:
+        model = Like
+        fields = "__all__"
+        read_only_fields = ["user"]
+
+    def create(self, validated_data):
+        """Overriding the create method to associate the authenticated user with the like"""
+        validated_data["user"] = self.context["request"].user  # Set the user as the logged-in user
+        return super().create(validated_data)
+
+
+class RatingSerializer(serializers.ModelSerializer):
+    user = serializers.StringRelatedField(read_only=True)
+    post_title = serializers.CharField(source="post.title", read_only=True)
+
+    class Meta:
+        model = Rating
+        fields = "__all__"
+
+    def create(self, validated_data):
+        validated_data["user"] = self.context["request"].user
+        return super().create(validated_data)
