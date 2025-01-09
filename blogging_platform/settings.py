9f7f32d6c25af547a643b203f2a2c1da2b4a3057
Mr-waweru <gitaugeorge001@gmail.com>
2025-01-09 19:43:20 +0300

    Add local host in allowed hosts

--- blogging_platform/settings.py
+++ blogging_platform/settings.py
@@ -23,7 +23,10 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env("DEBUG")
 
-ALLOWED_HOSTS = ["eigroeg.pythonanywhere.com"]
+ALLOWED_HOSTS = [
+    "eigroeg.pythonanywhere.com",
+    "localhost",
+]
 
 
 # Application definition
